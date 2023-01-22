e239acd062020e8bec4b58b0340787e5a53a67cc
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-01-22 16:57:00 +0100

    description application in programs

--- DZ_RoutingFromOtherOrdner_2/Program.cs
+++ DZ_RoutingFromOtherOrdner_2/Program.cs
@@ -5,6 +5,7 @@ var app = builder.Build();
 
 app.UseMyErrorHandling();
 //app.UseMyMiddleware();
-app.UseStaticFiles();
+//use or app.UseMyMiddleware() or this if is there file wwwroot with html_file
+app.UseStaticFiles(); //it see wwwroot order
 
 app.Run();
